2e8bd22bef5168d231589a42f9e6138f94de1f4d
Artem Matvieientsev <tmatveentsev@gmail.com>
2025-03-19 20:45:52 +0100

    starts files change

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Toaster } from "react-hot-toast";
 import "./index.css";
-import App from "./App.jsx";
+import App from "./App";
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
     <>
-      <Toaster position="top-right" reverseOrder={false} />
       <App />
     </>
   </StrictMode>
